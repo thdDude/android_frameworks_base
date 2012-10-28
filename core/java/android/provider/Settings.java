92bdaf4ef9823a40274470cd736db48501b4a68a
DerTeufel <dominik-kassel@gmx.de>
2012-10-29 00:55:50 +0100

    playing with colors...
    Change-Id: I7230edf9be284e62b38ec74ab9505ad79e6d59fc
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2620,6 +2620,20 @@ public final class Settings {
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 
         /**
+         * Notification Toggles Icon Color (On)
+         *
+         * @hide
+         */
+        public static final String TOGGLE_ICON_ON_COLOR = "toggle_icon_color_on";
+
+        /**
+         * Notification Toggles Icon Color (Off)
+         *
+         * @hide
+         */
+        public static final String TOGGLE_ICON_OFF_COLOR = "toggle_icon_color_off";
+
+        /**
          * Use the Notification Power Widget? (Who wouldn't!)
          *
          * @hide

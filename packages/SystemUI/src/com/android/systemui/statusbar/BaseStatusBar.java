eeb130d70b602f835f985f39448e6304d13a793c
Lars Greiss <kufikugel@googlemail.com>
2013-04-17 21:06:12 +0200

    Frameworks: kufikugel should not work with headaches
    small correction....now all triggers are disabled on
    Lockscreen like it should
    
    cheers
    
    Change-Id: I33b23e082135662635eb304726ae075ad26a1f41

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -125,6 +125,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected static final boolean ENABLE_INTRUDERS = false;
 
     private boolean mPieShowTrigger = false;
+    private boolean mDisableTriggers = false;
     private float mPieTriggerSize;
 
     private boolean mPieImeIsShowing = false;
@@ -1659,7 +1660,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private void attachPie() {
         if (isPieEnabled()) {
-            setupTriggers(false, false);
+            setupTriggers(false);
             // Create our container, if it does not exist already
             if (mPieContainer == null) {
                 mPieContainer = new PieLayout(mContext);
@@ -1702,8 +1703,13 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     }
 
-    public void setupTriggers(boolean forceDisableBottomAndTopTrigger, boolean disableTriggers) {
-            if (disableTriggers) {
+    public void disableTriggers( boolean disableTriggers) {
+        mDisableTriggers = disableTriggers;
+        setupTriggers(false);
+    }
+
+    public void setupTriggers(boolean forceDisableBottomAndTopTrigger) {
+            if (mDisableTriggers) {
                 updatePieTriggerMask(0);
                 return;
             }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -167,7 +167,7 @@ public class PhoneStatusBarView extends PanelBar {
         mLastFullyOpenedPanel = null;
 
         // show up you pie controls
-        mBar.setupTriggers(false, false);
+        mBar.setupTriggers(false);
 
     }
 
@@ -179,7 +179,7 @@ public class PhoneStatusBarView extends PanelBar {
         }
 
         // back off you pie controls!
-        mBar.setupTriggers(true, false);
+        mBar.setupTriggers(true);
 
         mFadingPanel = openPanel;
         mLastFullyOpenedPanel = openPanel;

ec1d122ebba79e6f64d921cbbe519aecee40d8dc
BigBrother1984 <stevewatersy@gmail.com>
2013-06-17 11:12:14 +0200

    Fix Halo not showed at first notifiction drop
    Change-Id: Iefa98e2168e0c53db99466de69da91cfd3465155

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -514,6 +514,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected void updateHaloButton() {
         if (!mHaloEnabled) {
             mHaloButtonVisible = false;
+        } else {
+            mHaloButtonVisible = true; 
         }
         if (mHaloButton != null) {
             mHaloButton.setVisibility(mHaloButtonVisible && !mHaloActive ? View.VISIBLE : View.GONE);

ccc267405b094c63ae93d9c0840c4b6f6e6fdb8b
DerTeufel <dominik-kassel@gmx.de>
2013-07-01 15:26:56 +0200

    fix
    Change-Id: If7d0ec696a530566ade455bb3984c95e20779836

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2908,9 +2908,6 @@ public class PhoneStatusBar extends BaseStatusBar {
         makeStatusBarView();
         repositionNavigationBar();
 
-        if (mNavigationBarView != null)
-            mNavigationBarView.updateResources();
-
         // recreate StatusBarIconViews.
         for (int i = 0; i < nIcons; i++) {
             StatusBarIcon icon = icons.get(i);

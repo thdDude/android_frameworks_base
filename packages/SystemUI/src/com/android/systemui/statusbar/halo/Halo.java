f0bd34fce5032bcbd145b679435b79c30ca6ca66
Scott Brady <sbradymobile@gmail.com>
2013-07-03 17:35:54 -0400

    Unattach halo observer on window state change
    Change-Id: Ib817dbb401169cbc7022430f93398181d007810e

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -150,6 +150,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
     private Paint mPaintWhite = new Paint();
     private Paint mPaintHoloRed = new Paint();
 
+    private boolean mAttached = false;
     private boolean isBeingDragged = false;
     private boolean mHapticFeedback;
     private boolean mHideTicker;
@@ -159,6 +160,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
     private boolean mIsNotificationNew = true;
     private boolean mOverX = false;
     private boolean mInteractionReversed = true;
+    private boolean hiddenState = false;
 
     private int mIconSize, mIconHalfSize;
     private int mScreenWidth, mScreenHeight;
@@ -168,7 +170,6 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
     private int oldIconIndex = -1;
     private float initialX = 0;
     private float initialY = 0;
-    private boolean hiddenState = false;
 
     private final class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
@@ -205,6 +206,28 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
         }
     }
 
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        if (!mAttached) {
+            mAttached = true;
+            mSettingsObserver = new SettingsObserver(new Handler());
+            mSettingsObserver.observe();
+            mSettingsObserver.onChange(true);
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+        if (mAttached) {
+            mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
+            mAttached = false;
+        }
+    }
+
     public Halo(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
@@ -222,10 +245,6 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
         mGestureDetector = new GestureDetector(mContext, new GestureListener());
         mHandler = new Handler();
         mRoot = this;
-
-        mSettingsObserver = new SettingsObserver(new Handler());
-        mSettingsObserver.observe();
-        mSettingsObserver.onChange(true);
 
         // Init variables
         BitmapDrawable bd = (BitmapDrawable) mContext.getResources().getDrawable(R.drawable.halo_bg);

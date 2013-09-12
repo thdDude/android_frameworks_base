5edc56c1eb2041908eef1143eac90b8536dae38b
Schubi <schubi@erlangen.ccc.de>
2013-09-12 15:25:11 +0200

    Use max 8 QS tiles in landscape, not 10
    Change-Id: I8283311a1cf7210bd299c55b5188cd6a294746c6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsContainerView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsContainerView.java
@@ -61,10 +61,14 @@ public class QuickSettingsContainerView extends FrameLayout {
     void updateResources() {
         Resources r = getContext().getResources();
         mCellGap = r.getDimension(R.dimen.quick_settings_cell_gap);
+        
+        //Default for portrait is 3, for landscape 6.
+        //When 5 is set for portrait, which is two more than default,
+        //use 8 on landscape mode, which is also two more than default.  
         mNumColumns = Settings.System.getInt(getContext().getContentResolver(),
                 Settings.System.QUICK_SETTINGS_COLUMNS, 3);
         if (isLandscape()) {
-            mNumColumns = mNumColumns * 2;
+            mNumColumns = 6 + (mNumColumns - 3);
         }
         requestLayout();
     }

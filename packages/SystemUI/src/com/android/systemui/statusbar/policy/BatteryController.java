38d390728126728d82355a65bb9c1f2c2341f96e
cristianomatos <cristianobmatos@gmail.com>
2013-09-13 06:28:05 -0300

    Changes to fit new HALO Mods implementation
    Change-Id: I340b2cf0293d2605300c8d51af28ecbab9b968e0

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
@@ -89,8 +89,18 @@ public class BatteryController extends BroadcastReceiver {
     private ArrayList<BatteryStateChangeCallback> mChangeCallbacks =
             new ArrayList<BatteryStateChangeCallback>();
 
+    // For HALO Mods
+    private ArrayList<BatteryStateChangeCallbackHalo> mChangeCallbacksHalo =
+            new ArrayList<BatteryStateChangeCallbackHalo>();
+	
+
     public interface BatteryStateChangeCallback {
         public void onBatteryLevelChanged(int level, int status);
+    }
+
+    //For HALO Mods
+    public interface BatteryStateChangeCallbackHalo {
+        public void onBatteryLevelChangedHalo(int level, boolean pluggedIn);
     }
 
     public BatteryController(Context context) {
@@ -130,6 +140,16 @@ public class BatteryController extends BroadcastReceiver {
     public void removeStateChangedCallback(BatteryStateChangeCallback cb) {
         mChangeCallbacks.remove(cb);
     }
+
+    // For Halo Mods
+    public void addStateChangedCallbackHalo(BatteryStateChangeCallbackHalo cb_Halo) {
+        mChangeCallbacksHalo.add(cb_Halo);
+    }
+
+    // For Halo Mods
+    public void removeStateChangedCallbackHalo(BatteryStateChangeCallbackHalo cb_Halo) {
+        mChangeCallbacksHalo.remove(cb_Halo);
+    }	
 
     // Allow override battery icons
     public int getIconStyleUnknown() {
@@ -214,6 +234,11 @@ public class BatteryController extends BroadcastReceiver {
 
         for (BatteryStateChangeCallback cb : mChangeCallbacks) {
             cb.onBatteryLevelChanged(level, getBatteryStatus());
+        }
+
+	// For HALO Mods
+	for (BatteryStateChangeCallbackHalo cb_Halo : mChangeCallbacksHalo) {
+            cb_Halo.onBatteryLevelChangedHalo(mBatteryLevel, mBatteryPlugged);
         }
     }
 

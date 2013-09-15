8543ff253e64f9c4403572aebc9d4fa23e28e2a1
DerTeufel <dominik-kassel@gmx.de>
2013-09-15 21:22:32 +0200

    custom lockscreen background: only apply additional color filter, if not using optimus4 color
    Change-Id: I7b97eae0552cf06bc007036b9de426d5411c268b

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -486,8 +486,10 @@ public class KeyguardHostView extends KeyguardViewBase {
             }
         }
         if (back != null) {
+	    if(!mUseOp4Lockscreen || !mUseOp4Color) {
             back.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_ATOP);
             setBackground(back);
+	    }
         }
     }
 
